2b90c8b8a21fd1333aa31861504d6050564a7b4b
Majed Bamardouf <majedtb12@gmail.com>
2023-03-28 23:27:32 +0300

    change 1

---
+++ scripts/browse.js
@@ -0,0 +1,60 @@
+
+function showPhoneBrands() {
+    var checkbox = ( document.getElementById('phone')) ;
+
+    const box = document.getElementById('phone-brands-filter');
+    
+      if (checkbox.checked) {
+        box.style.display = 'block';
+      } else {
+        box.style.display = 'none';
+      }
+}
+
+function showMonitorBrands() {
+  var checkbox = ( document.getElementById('monitor')) ;
+
+  const box = document.getElementById('monitor-brands-filter');
+  
+    if (checkbox.checked) {
+      box.style.display = 'block';
+    } else {
+      box.style.display = 'none';
+    }
+}
+
+function showHeadphoneBrands() {
+  var checkbox = ( document.getElementById('headphone')) ;
+
+  const box = document.getElementById('headphone-brands-filter');
+  
+    if (checkbox.checked) {
+      box.style.display = 'block';
+    } else {
+      box.style.display = 'none';
+    }
+}
+
+function showMouseBrands() {
+  var checkbox = ( document.getElementById('mouse')) ;
+
+  const box = document.getElementById('mouse-brands-filter');
+  
+    if (checkbox.checked) {
+      box.style.display = 'block';
+    } else {
+      box.style.display = 'none';
+    }
+}
+
+function showKeyboardBrands() {
+  var checkbox = ( document.getElementById('keyboard')) ;
+
+  const box = document.getElementById('keyboard-brands-filter');
+  
+    if (checkbox.checked) {
+      box.style.display = 'block';
+    } else {
+      box.style.display = 'none';
+    }
+}
